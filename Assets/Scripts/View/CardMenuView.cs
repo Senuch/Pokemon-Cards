19388b29f258a1bcce2ee5ca901e4424f200929f
Jerry <s.uzairtariq@gmail.com>
2023-05-16 01:01:49 +0500

    Added views for rendering pokemon cards (#3)
    1. Added TextMesh Pro
    2. Pokemon card view
    3. Pokemon card menu view

---
+++ Assets/Scripts/View/CardMenuView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace View
+{
+    public class CardMenuView : MonoBehaviour
+    {
+        public Button next;
+        public Button previous;
+        public GameObject pokemonCardPrefab;
+        public GameObject pokeCardMenuDisplay;
+    }
+}
