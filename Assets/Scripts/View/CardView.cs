03799462c7dc490228051a9fdbd80f58d56d4f35
Senuch <s.uzairtariq@gmail.com>
2023-05-16 00:58:16 +0500

    Added views for rendering pokemon cards

---
+++ Assets/Scripts/View/CardView.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+namespace View
+{
+    public class CardView : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI name;
+        [SerializeField] private TextMeshProUGUI baseExperience;
+    }
+}
