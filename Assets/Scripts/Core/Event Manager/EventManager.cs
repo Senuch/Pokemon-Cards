4c2197cfae2dad3bbd0a0508b1dbef1ee6e391fa
Senuch <s.uzairtariq@gmail.com>
2023-05-17 02:20:04 +0500

    Added Event Manager

---
+++ Assets/Scripts/Core/Event Manager/EventManager.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+
+namespace Core.Event_Manager
+{
+    public class EventManager
+    {
+        private static EventManager _instance;
+        public delegate void EventCallback(params object[] args);
+        private readonly Dictionary<string, EventCallback> _events = new ();
+
+        public static EventManager Instance
+        {
+            get
+            {
+                if (_instance is null)
+                {
+                    _instance = new EventManager();
+                }
+
+                return _instance;
+            }
+        }
+
+        private EventManager(){}
+
+        public void Subscribe(string eventName, EventCallback callback)
+        {
+            if (_events.ContainsKey(eventName))
+            {
+                _events[eventName] += callback;
+                return;
+            }
+
+            _events.Add(eventName, callback);
+        }
+
+        public void Unsubscribe(string eventName, EventCallback callback)
+        {
+            if (_events.ContainsKey(eventName))
+            {
+                _events[eventName] -= callback;
+            }
+        }
+
+        public void Trigger(string eventName, params object[] args)
+        {
+            if (_events.TryGetValue(eventName, out var @event))
+            {
+                @event.Invoke(args);
+            }
+        }
+    }
+}
